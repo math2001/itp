0bafcdc150d223c25450ef679a6c6e24ef8b8f9f
Mathieu PATUREL <australie.p@gmail.com>
2017-09-24 18:19:51 +1000

    :memo: add help message

--- index.js
+++ index.js
@@ -12,10 +12,15 @@ function listTemplates(from) {
 }
 
 function main(files) {
+    if (files.indexOf('--help') !== 1) {
+        console.error('$ template files...')
+        console.error('\nCopies files from ~/template/ to your current working directory')
+        return
+    }
     const CWD = process.cwd()
     for (let name of files) {
         try {
-            cp.sync(TEMPLATES_DIR + name, to)
+            cp.sync(TEMPLATES_DIR + name, CWD + '/' + name)
         } catch (err) {
             console.error(`Could not copy the template ${name}`)
             console.error(err)
